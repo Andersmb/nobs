6e705957ec76817fc4840ed6a1173d7d2b1b1332
Andersmb <anders.brakestad@gmail.com>
2020-04-30 10:09:30 +0200

    minor cleanup

--- plot_bsse.py
+++ plot_bsse.py
@@ -11,20 +11,19 @@ basis_sets = OrderedDict({"def2qzvpp": "def2-qzvpp",
                           "aug-6311gdp": "6-311+g(d,p)",
                           "6311gdp": "6-311g(d,p)"})
 
-fig, axes = plt.subplots(nrows=2, figsize=(10, 7.5), dpi=100, sharex="col")
-
+fig, axes = plt.subplots(nrows=2, figsize=(10, 7.5), dpi=100, sharex="all")
+no_reactions = 16
 width = 0.2
 colors = ["crimson", "lightblue", "blue", "lightgreen"]
 lw = 1.25
 ec = "black"
 fs = 16
-ind = np.arange(16)
+ind = np.arange(no_reactions)
 cntr = 0
 for ax, func in zip(axes, data.keys()):
     i = 0
     for baskey, basval in basis_sets.items():
         rxn, bas = zip(*data[func][baskey])
-
         ax.bar(ind+i*width, bas, width, color=colors[i], edgecolor=ec, linewidth=lw, label=basval)
         i += 1
 
@@ -33,14 +32,13 @@ for ax, func in zip(axes, data.keys()):
 
     ax.set_ylabel(r'BSSE $\left[ \frac{kcal}{mol} \right]$', fontsize=fs)
     ax.set_title(f"Functional: {func}", fontsize=fs)
-    [ax.tick_params(dim, labelsize=fs-4) for dim in ("x", "y")]
+    [ax.tick_params(dim, labelsize=fs-2) for dim in ("x", "y")]
     ax.grid(axis="y")
 
-    if cntr == 0: ax.legend(fontsize=fs, ncol=1)
+    if cntr == 0:
+        ax.legend(fontsize=fs, ncol=1)
     cntr += 1
 
 plt.tight_layout()
 plt.savefig("figs/bsse.png")
-#plt.show()
-
-
+# plt.show()
