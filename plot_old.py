220e4b11d4440381f5e245db924d8b2b06c40691
Andersmb <anders.brakestad@gmail.com>
2020-05-01 19:01:13 +0200

    make plot of relative error, gto vs mw

---
+++ plot_old.py
@@ -0,0 +1,55 @@
+import yaml
+import numpy as np
+from collections import OrderedDict
+from pprint import pprint
+import matplotlib.pyplot as plt
+from functions import get_old_data, get_old_mwref
+
+
+def plot_rxn_energy(cp=True):
+    GTO = get_old_data()
+    REF = get_old_mwref()
+    basis_sets = OrderedDict({"def2qzvpp": "def2-qzvpp",
+                              "def2tzvp": "def2-tzvp",
+                              "aug-6311gdp": "6-311+g(d,p)"})
+
+    fig, axes = plt.subplots(nrows=2, figsize=(10, 7.5), dpi=100, sharex="all")
+    no_reactions = 7
+    no_basis = 3
+    width = 1/(no_basis+1)
+    colors = ["crimson", "lightblue", "blue"]
+    lw = 1.25
+    ec = "black"
+    fs = 16
+    ind = np.arange(no_reactions)
+    cntr = 0
+
+    for ax, func in zip(axes, GTO.keys()):
+        i = 0
+        for baskey, basval in basis_sets.items():
+            rxn, gto, gto_cp = zip(*GTO[func][baskey])
+            rxn, mw = zip(*REF[func])
+            re = (np.asarray(gto) - np.asarray(mw)) / np.asarray(mw) * 100
+            re_cp = (np.asarray(gto_cp) - np.asarray(mw)) / np.asarray(mw) * 100
+
+            ax.bar(ind + i * width, re_cp, width, color=colors[i], edgecolor=ec, linewidth=lw, label=basval)
+            i += 1
+
+        ax.set_xticks(ind + (no_basis-1) / 2 * width)
+        ax.set_xticklabels(rxn)
+
+        ax.set_ylabel('Relative Error [%]', fontsize=fs)
+        ax.set_title(f"Functional: {func}", fontsize=fs)
+        [ax.tick_params(dim, labelsize=fs - 2) for dim in ("x", "y")]
+        ax.grid(axis="y")
+
+        if cntr == 0:
+            ax.legend(fontsize=fs, ncol=1)
+        cntr += 1
+
+    plt.tight_layout()
+    plt.savefig("figs/old_gto_vs_mw_cp.png")
+    #plt.show()
+
+
+plot_rxn_energy()
