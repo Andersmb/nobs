d654fe7594c9c4085a1f608355157f20c6a858da
Andersmb <anders.brakestad@gmail.com>
2020-05-13 12:54:11 +0200

    code cleanup, more basis sets

--- plot_bsse.py
+++ plot_bsse.py
@@ -1,6 +1,7 @@
 import yaml
 import numpy as np
 from collections import OrderedDict
+from tqdm import tqdm, trange
 import matplotlib.pyplot as plt
 
 with open("data_sets/bsse.yaml") as f:
@@ -8,35 +9,39 @@ with open("data_sets/bsse.yaml") as f:
 
 basis_sets = OrderedDict({"def2qzvpp": "def2-qzvpp",
                           "def2tzvp": "def2-tzvp",
+                          "def2svp": "def2-svp",
                           "aug-6311gdp": "6-311+g(d,p)",
-                          "6311gdp": "6-311g(d,p)"})
+                          "6311gdp": "6-311g(d,p),",
+                          "631g": "6-31g"})
 
-fig, axes = plt.subplots(nrows=2, figsize=(10, 7.5), dpi=100, sharex="all")
-no_reactions = 16
-width = 0.2
-colors = ["crimson", "lightblue", "blue", "lightgreen"]
+fig, axes = plt.subplots(nrows=2, figsize=(10, 5), dpi=100, sharex="all")
+n_rxn = len(data["bp86"]["def2svp"])
+n_basis = len(data["bp86"])
+width = 1/(n_basis+2)
+colors = ["crimson", "lightblue", "blue", "lightgreen", "silver", "goldenrod"]
 lw = 1.25
 ec = "black"
 fs = 16
-ind = np.arange(no_reactions)
+pos = np.arange(n_rxn)
 cntr = 0
+
 for ax, func in zip(axes, data.keys()):
     i = 0
     for baskey, basval in basis_sets.items():
         rxn, bas = zip(*data[func][baskey])
-        ax.bar(ind+i*width, bas, width, color=colors[i], edgecolor=ec, linewidth=lw, label=basval)
+        ax.bar(pos+i*width, bas, width, color=colors[i], edgecolor=ec, linewidth=lw, label=basval)
         i += 1
 
-    ax.set_xticks(ind + 3/2*width)
+    ax.set_xticks(pos + n_basis/2*width)
     ax.set_xticklabels(rxn)
 
     ax.set_ylabel(r'BSSE $\left[ \frac{kcal}{mol} \right]$', fontsize=fs)
     ax.set_title(f"Functional: {func}", fontsize=fs)
-    [ax.tick_params(dim, labelsize=fs-2) for dim in ("x", "y")]
+    [ax.tick_params(dim, labelsize=fs-3) for dim in ("x", "y")]
     ax.grid(axis="y")
 
     if cntr == 0:
-        ax.legend(fontsize=fs, ncol=1)
+        ax.legend(fontsize=fs-3, ncol=3)
     cntr += 1
 
 plt.tight_layout()
